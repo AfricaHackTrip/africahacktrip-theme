218814bfa204d3b4a4ee2389efe89f457a2ddc8a
Tobias Kräntzer <info@tobias-kraentzer.de>
2013-08-07 21:02:00 +0200

    Add support (to the front page) for the infinit scroll plugin

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+		<?php if ( have_posts() ) : ?>
+
+            <section id="posts">
+    			<?php /* The loop */ ?>
+    			<?php while ( have_posts() ) : the_post(); ?>
+    				<?php get_template_part( 'content', get_post_format() ); ?>
+    			<?php endwhile; ?>
+            </section>
+            
+            <section id="paging-navigation">
+                <?php twentythirteen_paging_nav(); ?>
+            </section>
+
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
