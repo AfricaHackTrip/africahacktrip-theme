d8ec3becad873e99b9dd2d34f3f1fd19342d51d5
Michael Bumann <michael@railslove.com>
2013-09-12 01:50:25 +0300

    add jquery

--- header.php
+++ header.php
@@ -23,7 +23,8 @@
 	<meta name="viewport" content="width=device-width">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
