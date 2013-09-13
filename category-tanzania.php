f4728fb38b27f54625851f1c14592032a3a0cbba
Michael Bumann <michael@railslove.com>
2013-09-13 23:07:09 +0200

    more proper country content :)

--- category-tanzania.php
+++ category-tanzania.php
@@ -11,7 +11,7 @@
 
 get_header(); ?>
 
-<div class="row kenya">
+<div class="row tanzania">
   <main class="col-md-9">
     <section class="events">
       <div class="lanyrd-target-schedule">
@@ -31,19 +31,12 @@ get_header(); ?>
 
 
   </main>
-  <aside class="country col-md-3 kenya">
-    <h1>Nairobi</h1>
-    <h3>Sep 24 - Oct 27, 2013</h3>
-    <h2 class="flag">Kenya</h2>
+  <aside class="country col-md-3 tanzania">
+    <h1>Dar es Salaam</h1>
+    <h3>Oct 16 - Oct 23, 2013</h3>
+    <h2 class="flag">Tanzania</h2>
     <section class="country-info">
       <p>
-        Nairobi is East Africa's economic and innovation center. It may not be the fastest growing city in Africa, but it is home to a Creative Class, made up of Kenyans and expatriates alike, many of them working in ICT. From what we've heard so far, the city buzzes and hums with hacktivity.
-      </p>
-      <p>
-        In 2012, Kenya's ICT Board conceptualized a 5-year-plan to see ICT contribute 25% of the GDP and thus make Kenya a "digital nation". Key to this transition is innovation. More and more innovation takes place in hubs, where entrepreneurs cowork on finding solutions for local problems.
-      </p>
-      <p>
-        We are überexcited to come to Nairobi and meet the local hackers!
       </p>
     </section>
 
