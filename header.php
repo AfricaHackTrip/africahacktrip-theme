913d9f03c9cb8a25409b106784a9c344723f084c
Michael Bumann <michael@railslove.com>
2013-09-17 12:09:01 +0200

    logo style

--- header.php
+++ header.php
@@ -61,7 +61,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" style="background: transparent <?php echo get_theme_mod( 'africahacktrip_logo' ); ?> no-repeat;" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">AfricaHackTrip</a>
+            <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">AfricaHackTrip</a>
           </div>
           <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav navbar-nav pull-right' ) ); ?>
         </nav>
